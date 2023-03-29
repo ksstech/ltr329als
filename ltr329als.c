9941e6d9c53adf0967e6dd68380c39aec3fdf7a5
André Maree <ammaree@users.noreply.github.com>
2023-03-29 20:27:49 +0200

    Organise includes for simplicity

--- ltr329als.c
+++ ltr329als.c
@@ -3,10 +3,12 @@
  * Copyright (c) 2022 Andre M. Maree / KSS Technologies (Pty) Ltd.
  */
 
-#include "main.h"
+#include "hal_config.h"
+
 #if (halHAS_LTR329ALS > 0)
 #include "ltr329als.h"
 #include "endpoints.h"
+#include "options.h"
 #include "printfx.h"
 #include "rules.h"
 #include "syslog.h"
