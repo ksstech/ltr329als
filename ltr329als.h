bb8a9047038aa6077a7dbfaa3da286683208f5bb
André Maree <ammaree@users.noreply.github.com>
2023-09-28 10:13:28 +0200

    Update ltr329als.h

--- ltr329als.h
+++ ltr329als.h
@@ -35,71 +35,72 @@ enum {								// Status register bits
 // ######################################### Structures ############################################
 
 typedef struct __attribute__((packed)) {
-	uint8_t mode : 1;				// 0=Standby  1=Active
-	uint8_t	reset : 1;
-	uint8_t	gain : 3;
-	uint8_t	res : 3;
+	u8_t mode:1;				// 0=Standby  1=Active
+	u8_t reset:1;
+	u8_t gain:3;
+	u8_t res:3;
 } ltr329als_control_t;
 
 typedef struct __attribute__((packed)) {
-	uint8_t rate : 3;				// LSB
-	uint8_t	time : 3;
-	uint8_t	res : 2;
+	u8_t rate:3;				// LSB
+	u8_t time:3;
+	u8_t res:2;
 } ltr329als_meas_rate_t;
 
 typedef struct __attribute__((packed)) {
-	uint8_t rev : 4;				// LSB
-	uint8_t	part : 4;
+	u8_t rev:4;				// LSB
+	u8_t part:4;
 } ltr329als_part_id_t;
 
 typedef struct __attribute__((packed)) {
-	uint8_t res : 2;				// LSB
-	uint8_t	data : 1;
-	uint8_t intr : 1;				// don't care value ???
-	uint8_t gain : 3;
-	uint8_t valid :1;
+	u8_t res:2;				// LSB
+	u8_t data:1;
+	u8_t intr:1;				// don't care value ???
+	u8_t gain:3;
+	u8_t valid:1;
 } ltr329als_status_t;
 
 typedef struct __attribute__((packed)) {
 	union {
 		ltr329als_control_t control;
-		uint8_t CONTROL;
+		u8_t CONTROL;
 	};
 	union {
 		ltr329als_meas_rate_t meas_rate;
-		uint8_t MEAS_RATE;
+		u8_t MEAS_RATE;
 	};
 	union {
 		ltr329als_part_id_t part_id;
-		uint8_t PART_ID;
+		u8_t PART_ID;
 	};
-	uint8_t MANUFAC_ID;
+	u8_t MANUFAC_ID;
 	union {
 		union {							// Channel 1 -
-			uint16_t CH1;
-			uint8_t ch1[2];
+			u16_t CH1;
+			u8_t ch1[2];
 		};
 		union {							// Channel 0 -
-			uint16_t CH0;
-			uint8_t ch0[2];
+			u16_t CH0;
+			u8_t ch0[2];
 		};
-		uint8_t ch[4];
+		u8_t ch[4];
 	};
 	union {
 		ltr329als_status_t status;
-		uint8_t STATUS;
+		u8_t STATUS;
 	};
 } ltr329als_reg_t;
 DUMB_STATIC_ASSERT(sizeof(ltr329als_reg_t) == 9);
 
-typedef struct __attribute__((packed)) {				// SI70006/13/14/20/xx TMP & RH sensors
-	i2c_di_t *		psI2C;			// 4 bytes
+struct i2c_di_t;
+typedef struct __attribute__((packed)) ltr329als_t {	// SI70006/13/14/20/xx TMP & RH sensors
+	struct i2c_di_t * psI2C;			// 4 bytes
 	SemaphoreHandle_t mux;
 	union {
 		ltr329als_reg_t Reg;
-		uint8_t u8Buf[sizeof(ltr329als_reg_t)];
+		u8_t u8Buf[sizeof(ltr329als_reg_t)];
 	};
-	uint8_t	spare[3];
+	u8_t	spare[3];
 	#if (ltr329alsI2C_LOGIC == 3)		// 3 step read -> wait -> convert
 	TimerHandle_t th;
 	StaticTimer_t ts;
@@ -112,16 +113,14 @@ DUMB_STATIC_ASSERT(sizeof(ltr329als_t) == 20);
 
 // ###################################### Public functions #########################################
 
-struct rule_t;
-struct epw_t;
+int ltr329alsReadReg(u8_t Reg, u8_t * pRxBuf);
+int ltr329alsWriteReg(u8_t reg, u8_t val);
 
-int	ltr329alsIdentify(i2c_di_t * psI2C);
-int	ltr329alsConfig(i2c_di_t * psI2C);
-int	ltr329alsReConfig(i2c_di_t * psI2C);
-int	ltr329alsDiags(i2c_di_t * psI2C);
-int ltr329alsReportAll(report_t * psR);
-int	ltr329alsConfigMode (struct rule_t *, int Xcur, int Xmax);
-int	ltr329alsSense(struct epw_t * psEWP);
+int	ltr329alsIdentify(struct i2c_di_t * psI2C);
+int	ltr329alsConfig(struct i2c_di_t * psI2C);
+int	ltr329alsDiags(struct i2c_di_t * psI2C);
+struct report_t;
+int ltr329alsReportAll(struct report_t * psR);
 
 #ifdef __cplusplus
 	}
